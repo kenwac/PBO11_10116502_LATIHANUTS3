09ee0b57d98b71438a0b8c191531bf90e5cfba00
kenwac <32459292+kenwac@users.noreply.github.com>
2017-11-14 20:02:42 +0700

    Add files via upload

---
+++ PBO11_10116502_LATIHANUTS3/src/view/latihan3.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+/**
+ *
+ * @author ASUS
+ */
+class latihan3 {
+     private String nama, jenisKelamin, jurusan, minat, penempatan;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+
+    public String getMinat() {
+        return minat;
+    }
+
+    public void setMinat(String minat) {
+        this.minat = minat;
+    }
+
+    public String getPenempatan() {
+        return penempatan;
+    }
+
+    public void setPenempatan(String penempatan) {
+        this.penempatan = penempatan;
+    }
+    
+    
+    
+}
